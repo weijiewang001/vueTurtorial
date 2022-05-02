92f7bd1554a2f68cd14be728a83c5c147032bac6
Weijie Wang <65530009+weijiewang001@users.noreply.github.com>
2022-05-03 09:51:44 +1000

    Initializing firebase first

--- music-10-18-COMPLETED/src/main.js
+++ music-10-18-COMPLETED/src/main.js
@@ -3,14 +3,26 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import VeeValidatePlugin from './includes/validation';
-import './assets/tailwind.css';
+import { auth } from './assets/tailwind.css';
 import './assets/main.css';
 import './includes/firebase';
 
-const app = createApp(App);
 
-app.use(store);
-app.use(router);
-app.use(VeeValidatePlugin);
+let app;
 
-app.mount('#app');
+//callback function
+//check if the user is authenticated when the application starts/initialized.
+auth.onAuthStateChanaged(()=> {
+    //check if the app has been initialized
+    if (!app){
+        app = createApp(App);
+
+        app.use(store);
+        app.use(router);
+        app.use(VeeValidatePlugin);
+
+        app.mount('#app');
+    }
+});
+
+
