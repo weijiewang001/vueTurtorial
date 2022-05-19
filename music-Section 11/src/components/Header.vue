83acf2c871f19894276942c14bbcd9570a828d7b
Weijie Wang <65530009+weijiewang001@users.noreply.github.com>
2022-05-19 23:59:18 +1000

    custom links

--- music-Section 11/src/components/Header.vue
+++ music-Section 11/src/components/Header.vue
@@ -3,12 +3,19 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" 
+        to="/">
+        Music
+      </router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" to="/about">About</router-link>
+          </li>
+
           <!-- 检查如果store里面是登录状态了，隐藏按钮 -->
           <li v-if="!userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">
@@ -17,7 +24,7 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" to="/manage">Manage</router-link>
             </li>
             <li>
               <a class="px-2 text-white" @click.prevent="signout" href="#">Logout</a>
