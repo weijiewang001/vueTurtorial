f7e032d0bc0460dbb004cdcb7dcefe567d3b09c4
Weijie Wang <65530009+weijiewang001@users.noreply.github.com>
2022-05-31 10:06:20 +1000

    fallback

--- music-Section 11/src/components/Upload.vue
+++ music-Section 11/src/components/Upload.vue
@@ -19,6 +19,7 @@
                 @drop.prevent.stop="upload($event)">
                 <h5>Drop your files here</h5>
             </div>
+            <input type="file" multiple @change="upload($event)" />
             <hr class="my-6" />
             <div class="mb-4" v-for="upload in uploads" :key="upload.name">
                 <div class="font-bold text-sm" :class="upload.text_class">
@@ -46,7 +47,9 @@ export default {
     methods: {
         upload($event){
             this.is_dragover = false;
-            const files = [...$event.dataTransfer.files];
+            console.log("$event", $event)
+            // const files = [...$event.dataTransfer.files];
+            const files = $event.dataTransfer ? [...$event.dataTransfer.files] : [...$event.target.files];
             console.log(files);
 
             files.forEach((file) => {
