ee5ea0ce712229dff1c4a13927b2dc4ee852de26
Weijie Wang <65530009+weijiewang001@users.noreply.github.com>
2022-06-30 00:44:32 +1000

    Creating the Song Template:

--- music-14-1-start/src/components/SongItem.vue
+++ music-14-1-start/src/components/SongItem.vue
@@ -2,7 +2,8 @@
     <li class="flex justify-between items-center p-3 pl-6 cursor-pointer transition
         duration-300 hover:bg-gray-50">
         <div>
-        <a href="#" class="font-bold block text-gray-600">{{song.modified_name}}</a>
+        <!-- 通过router link 来路由重定向，然后通过路由动态传参数来传递id -->
+        <router-link :to="{ name: 'song', params: { id: song.docID } }" class="font-bold block text-gray-600">{{song.modified_name}}</router-link>
         <span class="text-gray-500 text-sm">{{song.display_name}}</span>
         </div>
 
