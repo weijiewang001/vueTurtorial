9ebac2af9533f9fdd1e1b6255733a9468470eda8
Weijie Wang <65530009+weijiewang001@users.noreply.github.com>
2022-07-15 01:04:41 +1000

    Directive Modifiers

--- music-15-1/src/directives/icon.js
+++ music-15-1/src/directives/icon.js
@@ -1,10 +1,21 @@
 export default{
   beforeMount(el, binding){
-    let iconClass = `fa fa-${binding.value} float-right text-green-400 text-2xl`;
+    // binding.value是绑定v-icon后面的icon type
+    let iconClass = `fa fa-${binding.value} text-2xl`;
 
+    // 绑定变量之后可以覆盖class
     if(binding.arg === 'full'){
       iconClass = binding.value;
+    }
 
+    if(binding.modifiers.right) {
+      iconClass += ' float-right';
+    }
+
+    if(binding.modifiers.yellow){
+      iconClass += ' text-yellow-400';
+    }else{
+      iconClass += ' text-green-400';
     }
 
     el.innerHTML += `<i class="${iconClass}"></i>`;
--- music-15-1/src/views/Home.vue
+++ music-15-1/src/views/Home.vue
@@ -24,7 +24,7 @@
   <section class="container mx-auto">
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200" 
-      v-icon="'headphones-alt'">
+      v-icon.right.yellow="'headphones-alt'">
         <span class="card-title">Songs</span>
 
       </div>
