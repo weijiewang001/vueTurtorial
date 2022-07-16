e3106c4618afd47b8c9506bf1e6a714d73c930c4
Weijie Wang <65530009+weijiewang001@users.noreply.github.com>
2022-07-16 11:54:49 +1000

    16-1

---
+++ music-16-1/src/main.js
@@ -0,0 +1,24 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import VeeValidatePlugin from './includes/validation';
+import { auth } from './includes/firebase';
+import './assets/tailwind.css';
+import './assets/main.css';
+import Icon from './directives/icon';
+
+let app;
+
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
+
+    app.use(store);
+    app.use(router);
+    app.use(VeeValidatePlugin);
+    app.directive('icon',Icon);
+
+    app.mount('#app');
+  }
+});
